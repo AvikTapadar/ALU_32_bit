c2124486fcc2758e7171c84352956b615593d682
Avik Tapadar <112395232+AvikTapadar@users.noreply.github.com>
2025-11-21 18:38:14 +0530

    made it more synthesizable

--- hw/rtl/alu_32.v
+++ hw/rtl/alu_32.v
@@ -4,37 +4,41 @@ module alu_32 #(
     input [w-1:0] a,b,
     input [3:0] alu_ctrl,
     output cout, neg, overflow, zero,
-    output reg [w-1 : 0]res
+    output reg [w-1 : 0] res
 );
     wire [w-1:0] add_res;
     wire add_overflow;
-    add_32 ins(.a(a), .b(b), .sub_in(alu_ctrl[0]), .res(add_res), .carry(cout), .overflow(add_overflow));
+    wire sub_mode;
 
-    assign overflow = (alu_ctrl <= 4'd1) ? add_overflow : 0; //this is to check the overflow from shift operations. Make them zero for shift operations
+    assign sub_mode = alu_ctrl[0] | (alu_ctrl == 4'd11) | (alu_ctrl == 4'd12);
+
+    add_32 ins(.a(a), .b(b), .sub_in(sub_mode), .res(add_res), .carry(cout), .overflow(add_overflow));
+
+    assign overflow = (alu_ctrl <= 4'd1) ? add_overflow : 1'b0; 
 
     always @(*) begin
         case(alu_ctrl)
-            4'b0000 : res = add_res; //lsb got to be 0 to add 
-            4'b0001 : res = add_res; //got to be 1 to sub
-            4'd2 : res = a&b; //AND
-            4'd3 : res = a|b; //OR
-            4'd4 : res = a^b; //XOR
-            4'd5 : res = a~^b; //XNOR;
-            4'd6 : res = ~(a|b); //NOR
-            4'd7 : res = ~(a&b); //NAND
-            4'd8 : res = a << b[4:0]; //shift logical left
-            4'd9 : res = a >> b[4:0]; //shift logical right
-            4'd10 : res = $signed(a) >>> b[4:0]; //arithmetic right shift 
-            4'd11 : res = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0; //Set less than (SLT)
-            4'd12 : res = (a<b) ? 32'd1 : 32'd0; //unsigned Set less than (SLTU)
-            4'd13 : res = (a == b) ? 32'd1 : 32'd0; //Set equal 
-            4'd14 : res = (a != b) ? 32'd1 : 32'd0; //set not equal
-            default : res = 0;
+            4'b0000 : res = add_res; // ADD
+            4'b0001 : res = add_res; // SUB
+            4'd2 : res = a & b; //AND
+            4'd3 : res = a | b; // OR
+            4'd4 : res = a ^ b; //XOR
+            4'd5 : res = a ~^ b; //XNOR
+            4'd6 : res = ~(a | b); //NOR
+            4'd7 : res = ~(a & b); //NAND
+            4'd8 : res = a << b[4:0]; // SLL
+            4'd9 : res = a >> b[4:0]; //SRL
+            4'd10 : res = $signed(a) >>> b[4:0]; //SRA
+            4'd11 : res = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0; //SLT
+            4'd12 : res = (a < b) ? 32'd1 : 32'd0; //SLTU
+            4'd13 : res = (a == b) ? 32'd1 : 32'd0; //BEQ
+            4'd14 : res = (a != b) ? 32'd1 : 32'd0; //BNEQ
+            default : res = 32'bx; //EASIER FOR SYNTHESIS
         endcase
     end
 
     assign zero = (res == 32'd0);
-    assign neg = (res[w-1]); //signed bit : 1 if result is negative
+    assign neg = (res[w-1]); 
     
 endmodule
 
--- hw/rtl/alu_ctrl.v
+++ hw/rtl/alu_ctrl.v
@@ -6,35 +6,36 @@ module alu_ctrl_32 (
 );
 
     always @(*) begin
+        alu_ctrl = 4'd0; 
+
         case(aluop)
-            2'b00 : alu_ctrl = 4'd0; //LOAD / STORE
-            2'b01 : begin //BRANCH INSTRUCTIONS
+            2'b00 : alu_ctrl = 4'd0; // LOAD / STORE (ADD)
+            2'b01 : begin // BRANCH INSTRUCTIONS
                 case(func3)
-                    3'b000: alu_ctrl = 4'd13; 
-                    3'b001: alu_ctrl = 4'd14; 
-                    3'b100: alu_ctrl = 4'd11; 
-                    3'b101: alu_ctrl = 4'd12; 
-                    3'b110: alu_ctrl = 4'd12; // BLTU, basically checks SLTU to get the output
-                    3'b111: alu_ctrl = 4'd12; // BGEU, same as above to get the output
+                    3'b000: alu_ctrl = 4'd13; // BEQ
+                    3'b001: alu_ctrl = 4'd14; // BNEQ
+                    3'b100: alu_ctrl = 4'd11; // BLT
+                    3'b101: alu_ctrl = 4'd12; // BGE
+                    3'b110: alu_ctrl = 4'd12; // BLTU 
+                    3'b111: alu_ctrl = 4'd12; // BGEU 
                     default: alu_ctrl = 4'd0;
                 endcase
             end
-            2'b10 : begin //R-INSTRUCTIONS
+            2'b10 : begin // R-TYPE INSTRUCTIONS
                 case(func3)
-                    3'b000: alu_ctrl = (func7[5]) ? 4'd1 : 4'd0;
-                    3'b111: alu_ctrl = 4'd2;
-                    3'b110: alu_ctrl = 4'd3; 
-                    3'b100: alu_ctrl = 4'd4;
-                    3'b001: alu_ctrl = 4'd8; 
-                    3'b101: alu_ctrl = (func7[5]) ? 4'd10 : 4'd9;
-                    3'b010: alu_ctrl = 4'd11;
-                    3'b011: alu_ctrl = 4'd12; 
+                    3'b000: alu_ctrl = (func7[5]) ? 4'd1 : 4'd0; // SUB : ADD
+                    3'b111: alu_ctrl = 4'd2; // AND
+                    3'b110: alu_ctrl = 4'd3; // OR
+                    3'b100: alu_ctrl = 4'd4; // XOR
+                    3'b001: alu_ctrl = 4'd8; // SLL
+                    3'b101: alu_ctrl = (func7[5]) ? 4'd10 : 4'd9; // SRA : SRL
+                    3'b010: alu_ctrl = 4'd11; // SLT
+                    3'b011: alu_ctrl = 4'd12; // SLTU
                     default: alu_ctrl = 4'd0;
                 endcase
             end
             default: alu_ctrl = 4'd0;
         endcase
-            
     end
     
 endmodule
